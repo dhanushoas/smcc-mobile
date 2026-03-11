1d2afff8254b8b927adc00f8a5acf7b3f0590325
dhanush oas <dhanushoas@example.com>
2026-03-11 09:00:24 +0530

    fix

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -313,7 +313,8 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
                       ),
                     ],
                   ),
-                ],
+                ),
+              ],
               ),
             ),
 
